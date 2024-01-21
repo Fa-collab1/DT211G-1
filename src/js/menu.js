e6d6ebae4dc6e12e367ba252d727fcb1ab3c4abb
Fa-collab1 <opus.ionam@gmail.com>
2024-01-21 03:34:26 +0100

    css för bildsidan + kommentarer i js och css-kod

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,12 @@
+// Lägger till en eventlyssnare för klick på elementet med ID "menuButton"
 document.getElementById("menuButton").addEventListener("click", function() {
+    // Hämtar dropdown-innehållet
     var dropdownContent = document.getElementById("dropdownContent");
+
+    // Kontrollerar om dropdown-menyn redan är synlig
     if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
+        dropdownContent.style.display = "none"; // Gömmer menyn om den är synlig
     } else {
-        dropdownContent.style.display = "block";
+        dropdownContent.style.display = "block"; // Visar menyn om den är dold
     }
 });
